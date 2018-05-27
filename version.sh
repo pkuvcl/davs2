a3cb25e34847fb813c1888452923f0d6094778b1
luofl <falei.luo@gmail.com>
2018-05-27 18:48:58 +0800

    Fix the version string

--- version.sh
+++ version.sh
@@ -10,12 +10,10 @@
 
 # get version of remote and local repository
 VER_R=`git rev-list --count origin/master`
-VER_L=`git rev-list --count HEAD`
 VER_SHA=`git rev-parse HEAD | cut -c -16`
 
 # get API version from "./source/davs2.h"
 api=`grep '#define DAVS2_BUILD' < ./source/davs2.h | sed 's/^.* \([1-9][0-9]*\).*$/\1/'`
-apiver=`grep '#define DAVS2_API_VERSION' < ./source/davs2.h | sed 's/^.* \([1-9][0-9]*\).*$/\1/'`
 len=`expr length $api`
 end1=`expr $len - 1`
 VER_MAJOR=`echo $api | cut -c -$end1`
@@ -33,23 +31,22 @@ echo "// Author:  Falei LUO <falei.luo@gmail.com>"                              
 echo "//"                                                                             >> version.h
 echo "// ===========================================================================" >> version.h
 echo ""                                                                               >> version.h
-echo "#ifndef __DAVS2_VERSION_H__"                                                 >> version.h
-echo "#define __DAVS2_VERSION_H__"                                                 >> version.h
+echo "#ifndef DAVS2_VERSION_H"                                                        >> version.h
+echo "#define DAVS2_VERSION_H"                                                        >> version.h
 echo ""                                                                               >> version.h
 echo "// version number"                                                              >> version.h
 echo "#define VER_MAJOR         $VER_MAJOR     // major version number"               >> version.h
 echo "#define VER_MINOR         $VER_MINOR     // minor version number"               >> version.h
 echo "#define VER_BUILD         $VER_R    // build number"                            >> version.h
-echo "#define VER_TYPE          $apiver   // api type, 1: callback; 2: non-callback"  >> version.h
-echo "#define VER_SHA_STR \"$VER_SHA\"  // commit id"                                 >> version.h
+echo "#define VER_SHA_STR       \"$VER_SHA\"  // commit id"                           >> version.h
 echo ""                                                                               >> version.h
 echo "// stringify"                                                                   >> version.h
 echo "#define _TOSTR(x)       #x            // stringify x"                           >> version.h
 echo "#define TOSTR(x)        _TOSTR(x)     // stringify x, perform macro expansion"  >> version.h
 echo ""                                                                               >> version.h
-echo "// define XVERSION for resource file(*.rc)"                                     >> version.h
-echo "#define XVERSION        VER_MAJOR, VER_MINOR, VER_BUILD, VER_TYPE"              >> version.h
-echo "#define XVERSION_STR    TOSTR(VER_MAJOR) \".\" TOSTR(VER_MINOR) \".\" TOSTR(VER_BUILD) \".\" TOSTR(VER_TYPE) \" \" VER_SHA_STR" >> version.h
+echo "// define XVERSION string"                                                      >> version.h
+echo "#define XVERSION        VER_MAJOR, VER_MINOR, VER_BUILD"                        >> version.h
+echo "#define XVERSION_STR    TOSTR(VER_MAJOR) \".\" TOSTR(VER_MINOR) \".\" TOSTR(VER_BUILD) \" \" VER_SHA_STR" >> version.h
 echo "#define XBUILD_TIME     \"$BUILD_TIME\""                                        >> version.h
 echo ""                                                                               >> version.h
 echo "#endif // __VERSION_H__"                                                        >> version.h
@@ -57,5 +54,4 @@ echo "#endif // __VERSION_H__"                                                  
 mv version.h source/version.h
 
 # show version informations
-echo "#define DAVS2_VERSION \"r$api\""
-echo "#define DAVS2_POINTVER \"$VER_MAJOR.$VER_MINOR.$VER_R.$apiver\""
+echo "Defined davs2 version: r$api \"$VER_MAJOR.$VER_MINOR.$VER_R\",  BuildTime: $BUILD_TIME"
