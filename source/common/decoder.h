45ba7f7ef9858ddb86324826286514912e5727f2
hwren <hwrenx@126.com>
2018-09-17 17:23:12 +0800

    Rename with macro prefix(9): common/decoder.h

--- source/common/decoder.h
+++ source/common/decoder.h
@@ -38,18 +38,29 @@ extern "C" {
 
 #include "common.h"
 
+#define decoder_open FPFX(decoder_decoder_open)
 davs2_t *decoder_open(davs2_mgr_t *mgr, davs2_t *h, int idx_decoder);
+#define decoder_decode_picture_data FPFX(decoder_decode_picture_data)
 void *decoder_decode_picture_data(void *arg1, int arg2);
+#define decoder_close FPFX(decoder_decoder_close)
 void decoder_close(davs2_t *h);
+#define create_freepictures FPFX(create_freepictures)
 int  create_freepictures(davs2_mgr_t *mgr, int w, int h, int size);
+#define destroy_freepictures FPFX(destroy_freepictures)
 void destroy_freepictures(davs2_mgr_t *mgr);
+#define decoder_alloc_extra_buffer FPFX(decoder_alloc_extra_buffer)
 int  decoder_alloc_extra_buffer(davs2_t *h);
+#define decoder_free_extra_buffer FPFX(decoder_free_extra_buffer)
 void decoder_free_extra_buffer(davs2_t *h);
+#define davs2_write_a_frame FPFX(write_a_frame)
 void davs2_write_a_frame(davs2_picture_t *pic, davs2_frame_t *frame);
 
+#define task_get_references FPFX(task_get_references)
 int  task_get_references(davs2_t *h, int64_t pts, int64_t dts);
 
+#define task_unload_packet FPFX(task_unload_packet)
 void task_unload_packet(davs2_t *h, es_unit_t *es_unit);
+#define decoder_get_output FPFX(decoder_get_output)
 int decoder_get_output(davs2_mgr_t *mgr, davs2_seq_info_t *headerset, davs2_picture_t *out_frame, int is_flush);
 
 #ifdef __cplusplus
